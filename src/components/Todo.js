94e64247d23eeaf557e5c964547c1e2e22f75556
Lindsey <lindsey.lansford@gmail.com>
2023-03-27 12:33:34 -0600

    adding edit states

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,36 @@
 import React, { useState } from "react";
+import TodoForm from "./TodoForm";
 
 const Todo = (props) => {
 
+  const [edit, setEdit] = useState({
+    id: null,
+    value: '',
+  });
   
-};
+  const submitUpdate = (value) => {
+    props.editTodoItem(edit.is, value);
+    setEdit({ id: null, value: '' });
+  };
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate}/>
+  }
+
+  return props.todo.map((item, i) => {
+    <div
+      className={item.isCompelte}
+      key={i}
+    >
+      <div key={item.id} onClick={() => props.completeTodoItem(item.id)}>
+        {item.text}
+      </div>
+      <div className="icons">
+        <p onClick={() => setEdit({ id: item.id, value: item.text })}> ✏️</p>
+        <p onClick={() => props.removeTodoItem(item.id)}> 🗑️</p>
+      </div>
+    </div>
+  });
+}
 
 export default Todo;
