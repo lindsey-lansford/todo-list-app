165c97bb993bc254358bfd4bad8d7f3bd6888bf0
Lindsey <lindsey.lansford@gmail.com>
2023-03-27 12:41:27 -0600

    formatting

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,26 +2,22 @@ import React, { useState } from "react";
 import TodoForm from "./TodoForm";
 
 const Todo = (props) => {
-
   const [edit, setEdit] = useState({
     id: null,
-    value: '',
+    value: "",
   });
-  
+
   const submitUpdate = (value) => {
     props.editTodoItem(edit.id, value);
-    setEdit({ id: null, value: '' });
+    setEdit({ id: null, value: "" });
   };
 
   if (edit.id) {
-    return <TodoForm edit={edit} onSubmit={submitUpdate}/>
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
   }
 
   return props.todo.map((item, i) => {
-    <div
-      className={item.isCompelte}
-      key={i}
-    >
+    <div className={item.isCompelte} key={i}>
       <div key={item.id} onClick={() => props.completeTodoItem(item.id)}>
         {item.text}
       </div>
@@ -29,8 +25,8 @@ const Todo = (props) => {
         <p onClick={() => setEdit({ id: item.id, value: item.text })}> ✏️</p>
         <p onClick={() => props.removeTodoItem(item.id)}> 🗑️</p>
       </div>
-    </div>
+    </div>;
   });
-}
+};
 
 export default Todo;
