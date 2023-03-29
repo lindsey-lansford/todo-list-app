17cfed711cf7820b2dfbf82580334d6049134e39
Lindsey <lindsey.lansford@gmail.com>
2023-03-29 13:14:17 -0600

    adding priority into edits

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,13 +5,14 @@ const Todo = (props) => {
   const [edit, setEdit] = useState({
     id: null,
     value: "",
+    priority: '',
   });
 
   console.log(props.todo);
 
   const submitUpdate = (value) => {
     props.editTodoItem(edit.id, value);
-    setEdit({ id: null, value: "" });
+    setEdit({ id: null, value: "", priority: '' });
   };
 
   if (edit.id) {
@@ -19,12 +20,17 @@ const Todo = (props) => {
   }
 
   return props.todo.map((item, i) => (
-    <div className={item.isCompelte} key={i}>
+    <div
+      className={item.isCompelte
+      ? `bucket-row complete ${item.priority}`
+      : `bucket-row ${item.priority}`
+    }
+      key={i}>
       <div key={item.id} onClick={() => props.completeTodoItem(item.id)}>
         {item.text}
       </div>
       <div className="icons">
-        <p onClick={() => setEdit({ id: item.id, value: item.text })}> ✏️</p>
+        <p onClick={() => setEdit({ id: item.id, value: item.text, priority: item.priority })}> ✏️</p>
         <p onClick={() => props.removeTodoItem(item.id)}> 🗑️</p>
       </div>
     </div>
